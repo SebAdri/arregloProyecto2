6e1de9d323589d7f38a8d59dea875adaac38c89d
Sebas <sebascaballero77@gmail.com>
2019-04-05 05:46:38 -0400

    Sacando los head demas y puntos de commiteo
    Sacamos los head ara que se pueda levantar el proyecto

--- app/Empleado.php
+++ app/Empleado.php
@@ -8,13 +8,11 @@ class Empleado extends Model
 {
     protected $fillable = ['primerNombre', 'segundoNombre', 'primerApellido', 'segundoApellido', 'direccion', 'telefono', 'profesion_id'];
 
-<<<<<<< HEAD
     public function profesion(){
 		return $this->belongsTo(Profesion::Class);
-=======
+
     public function rubro(){
-	return $this->belongsTo(Rubro::Class);
->>>>>>> 12bf9022b901a4cbc29d6de78d1ade0d9f735312
+		return $this->belongsTo(Rubro::Class);
 	}
 
 	public function obras(){

8c7adf4dfec61607ed20b933e11c27ccae9853fe
Sebas <sebascaballero77@gmail.com>
2019-05-26 18:06:23 -0400

    agregue ruta de iconos de avance pero esta mal

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,11 +9,12 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="{{asset("css/style.css")}}">
 <link rel="stylesheet" type="text/css" href="{{asset("css/AdminLTE.css")}}">
-{{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> --}}
 <link rel="stylesheet" href="{{asset("css/select2.min.css")}}" rel="stylesheet" />
 <link rel="stylesheet" type="text/css" href="{{asset("css/dataTables.min.css")}}">
 <link rel="stylesheet" href="{{asset("css/select2.min.css")}}" rel="stylesheet" />
 <link rel="stylesheet" href="{{asset("css/bootstrap-toggle.min.css")}}" rel="stylesheet" />
+<!-- Ionicons -->
+<link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
 
 
 {{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js">
